bbea69b1234010bb81fb8ed67cf26614021cf5ea
Friederike Marby <friederike.marby@student.htw-berlin.de>
2022-12-21 15:31:57 +0100

    uploaded data + created id

--- 01_preprocessing.py
+++ 01_preprocessing.py
@@ -29,23 +29,27 @@ file_names = [
 ]
 
 results = []
+index = 0
 
 for file in file_names:
-    with open (file, "r") as json_file:
+    with open("old_data/"+file, "r") as json_file:
         data = json.load(json_file)
         
         for entry in data:
-            #format date
+            date = cleanDate(entry["last_online"]).isoformat()[:10]
+           
             results.append(
                 {
+                    "id": "{}_{}_{}_{}".format(index, date, entry["as_number"], entry["ip_address"]),
                     "ip_address": entry["ip_address"],
                     "as_number": entry["as_number"],
                     "as_name": entry["as_name"],
                     "country": entry["country"],
-                    "date": cleanDate(entry["last_online"]).isoformat()[:10],
+                    "date": date,
                 }
             )
+            index +=1
 
 
-with open ("all_old_data.json", "w") as json_file:
-    json.dump(results, fp=json_file)    
+with open("old_data/all_old_data.json", "w") as json_file:
+    json.dump(results, fp=json_file)
